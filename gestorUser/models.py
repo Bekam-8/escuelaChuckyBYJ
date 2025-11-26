6545217be0d3e8e9974e175bf8f6901df8a1f9b0
Bekam-8 <benjamin.contrersas50@inacapmail.cl>
2025-11-26 13:28:19 -0300

    agregar modelo GestorUser y configurar AUTH_USER_MODEL

--- gestorUser/models.py
+++ gestorUser/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class GestorUser(AbstractUser):
+    ROL_CHOICES = [
+        ('ADMIN', 'Administrador'),
+        ('NORMAL', 'Usuario Normal'),
+    ]
+
+    Fecha_nacimiento = models.DateField(null=True, blank=True)
+    rol = models.CharField(max_length=10, choices=ROL_CHOICES, default='NORMAL')
+
+    def __str__(self):
+        return f"{self.username} ({self.get_rol_display()})"
