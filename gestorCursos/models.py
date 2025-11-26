eb9efe7528a5b484fbbca9f67956f0ebe2d694c8
Corona9925 <jose.vasquez72@inacapmail.cl>
2025-11-26 13:29:36 -0300

    agregar modelos 'Curso' y 'Alumno' en el archivo models.py; crear archivo forms.py

--- gestorCursos/models.py
+++ gestorCursos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    codigo = models.CharField(max_length=10, unique=True)
+
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    fecha_nacimiento = models.CharField(max_length=10)
